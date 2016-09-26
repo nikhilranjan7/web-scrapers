3c24328f525796396fca35367a3665822f39e86c
nikhilranjan7 <nikhilranjan7@gmail.com>
2016-09-26 23:28:26 +0530

    Add text manual input method

--- typeracer.py
+++ typeracer.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
-import requests
+import requests, time
 from bs4 import BeautifulSoup
 
 browser = webdriver.Firefox()
 browser.get("http://play.typeracer.com/")
 
+time.sleep(5)
+
 practice = browser.find_element_by_link_text('Enter a typing race')
 practice.click()
 
-text = browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div[1]/div/span[2]")
+time.sleep(3)
+
+input_block = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input')
+
+
+text = input('What\'s the text?\n').split(' ')
+
+for word in text:
+    input_block.send_keys(word)
+    input_block.send_keys(' ')
